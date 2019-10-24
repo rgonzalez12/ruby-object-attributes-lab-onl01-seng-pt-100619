32facacf4c73240bb871facceb7b883eeab4cf48
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-24 04:37:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,13 @@
 class Dog 
-  
+  def initialize(dog_name, breed)
+    @dog_name = dog_name
+    @breed = breed
+  end
+ 
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
     
  
